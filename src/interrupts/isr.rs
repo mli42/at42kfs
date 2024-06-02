800e627675f548ed69172817c50adf9af7f42e0c
lsarrazi <saqrcheiron@gmail.com>
2024-06-02 17:38:55 +0000

    better handle keymapping (specifically for azerty) and also add numpad support

--- src/interrupts/isr.rs
+++ src/interrupts/isr.rs
@@ -87,6 +87,7 @@ pub static mut KEYBOARD_STATE: KeyboardState = KeyboardState {
     ctrl: false,
     alt: false,
     capslock: false,
+    verrnum: true,
 };
 
 pub static mut CLI_STATE: CliState = CliState {
--- src/keyboard/mod.rs
+++ src/keyboard/mod.rs
@@ -1,3 +1,5 @@
+use crate::println;
+
 macro_rules! create_keymap_array {
     // Match the pattern where specific values are provided at certain indices
     ($default:expr; $len:expr; $($index:expr => $value:expr),*) => {{
@@ -9,9 +11,9 @@ macro_rules! create_keymap_array {
     }};
 }
 
-const KEYMAP_US: [u8; 128] = create_keymap_array!(
+const KEYMAP_US: [u8; 0x300] = create_keymap_array!(
     0;
-    128;
+    0x300;
 
     0x1 => 0x1B,
     0x2 => '1' as u8,
@@ -64,12 +66,52 @@ const KEYMAP_US: [u8; 128] = create_keymap_array!(
     0x33 => ',' as u8,
     0x34 => '.' as u8,
     0x35 => '/' as u8,
-    0x39 => ' ' as u8
+    0x39 => ' ' as u8,
+
+    0x110 => 'Q' as u8,
+    0x111 => 'W' as u8,
+    0x112 => 'E' as u8,
+    0x113 => 'R' as u8,
+    0x114 => 'T' as u8,
+    0x115 => 'Y' as u8,
+    0x116 => 'U' as u8,
+    0x117 => 'I' as u8,
+    0x118 => 'O' as u8,
+    0x119 => 'P' as u8,
+    0x11A => '[' as u8,
+    0x11B => ']' as u8,
+
+    0x11E => 'A' as u8,
+    0x11F => 'S' as u8,
+    0x120 => 'D' as u8,
+    0x121 => 'F' as u8,
+    0x122 => 'G' as u8,
+    0x123 => 'H' as u8,
+    0x124 => 'J' as u8,
+    0x125 => 'K' as u8,
+    0x126 => 'L' as u8,
+    0x127 => ';' as u8,
+    0x128 => '\'' as u8,
+    0x129 => '`' as u8,
+
+    0x12B => '\\' as u8,
+    0x12C => 'Z' as u8,
+    0x12D => 'X' as u8,
+    0x12E => 'C' as u8,
+    0x12F => 'V' as u8,
+    0x130 => 'B' as u8,
+    0x131 => 'N' as u8,
+    0x132 => 'M' as u8,
+    0x133 => ',' as u8,
+    0x134 => '.' as u8,
+    0x135 => '/' as u8,
+    0x139 => ' ' as u8
+
 );
 
-const KEYMAP_FR: [u8; 128] = create_keymap_array!(
+const KEYMAP_FR: [u8; 0x300] = create_keymap_array!(
     0;
-    128;
+    0x300;
 
     0x1 => 0x1B,
     0x2 => '&' as u8,
@@ -84,6 +126,33 @@ const KEYMAP_FR: [u8; 128] = create_keymap_array!(
     0xB => 'à' as u8,
     0xC => ')' as u8,
     0xD => '=' as u8,
+
+    0x101 => 0x1B,
+    0x102 => '1' as u8,
+    0x103 => '2' as u8,
+    0x104 => '3' as u8,
+    0x105 => '4' as u8,
+    0x106 => '5' as u8,
+    0x107 => '6' as u8,
+    0x108 => '7' as u8,
+    0x109 => '8' as u8,
+    0x10A => '9' as u8,
+    0x10B => '0' as u8,
+    0x10C => '°' as u8,
+    0x10D => '+' as u8,
+
+    0x203 => '~' as u8,
+    0x204 => '#' as u8,
+    0x205 => '{' as u8,
+    0x206 => '[' as u8,
+    0x207 => '|' as u8,
+    0x208 => '`' as u8,
+    0x209 => '\\' as u8,
+    0x20A => '^' as u8,
+    0x20B => '@' as u8,
+    0x20C => ']' as u8,
+    0x20D => '}' as u8,
+
 
     0x10 => 'a' as u8,
     0x11 => 'z' as u8,
@@ -122,7 +191,59 @@ const KEYMAP_FR: [u8; 128] = create_keymap_array!(
     0x33 => ';' as u8,
     0x34 => ':' as u8,
     0x35 => '!' as u8,
-    0x39 => ' ' as u8
+    0x39 => ' ' as u8,
+
+    // numpad
+    0x52 => '0' as u8,
+    0x4F => '1' as u8,
+    0x50 => '2' as u8,
+    0x51 => '3' as u8,
+    0x4B => '4' as u8,
+    0x4C => '5' as u8,
+    0x4D => '6' as u8,
+    0x47 => '7' as u8,
+    0x48 => '8' as u8,
+    0x49 => '9' as u8,
+
+
+    0x110 => 'A' as u8,
+    0x111 => 'Z' as u8,
+    0x112 => 'E' as u8,
+    0x113 => 'R' as u8,
+    0x114 => 'T' as u8,
+    0x115 => 'Y' as u8,
+    0x116 => 'U' as u8,
+    0x117 => 'I' as u8,
+    0x118 => 'O' as u8,
+    0x119 => 'P' as u8,
+    0x11A => '^' as u8,
+    0x11B => '$' as u8,
+
+    0x11E => 'Q' as u8,
+    0x11F => 'S' as u8,
+    0x120 => 'D' as u8,
+    0x121 => 'F' as u8,
+    0x122 => 'G' as u8,
+    0x123 => 'H' as u8,
+    0x124 => 'J' as u8,
+    0x125 => 'K' as u8,
+    0x126 => 'L' as u8,
+    0x127 => 'M' as u8,
+    0x128 => '%' as u8,
+    0x129 => 'µ' as u8,
+
+    0x12B => '>' as u8,
+    0x12C => 'W' as u8,
+    0x12D => 'X' as u8,
+    0x12E => 'C' as u8,
+    0x12F => 'V' as u8,
+    0x130 => 'B' as u8,
+    0x131 => 'N' as u8,
+    0x132 => '?' as u8,
+    0x133 => '.' as u8,
+    0x134 => '/' as u8,
+    0x135 => '§' as u8,
+    0x139 => ' ' as u8
 );
 
 #[allow(dead_code)]
@@ -137,6 +258,7 @@ pub struct KeyboardState {
     pub ctrl: bool,
     pub alt: bool,
     pub capslock: bool,
+    pub verrnum: bool,
 }
 
 pub fn handle_scancode(scancode: u8, state: &mut KeyboardState, output: &mut [u8]) {
@@ -169,6 +291,11 @@ pub fn handle_scancode(scancode: u8, state: &mut KeyboardState, output: &mut [u8
                 write_change(0x08 as char);
             }
         }
+        0x45 => {
+            if is_pressed {
+                state.verrnum = !state.verrnum;
+            }
+        }
         _ => {
             if is_pressed {
                 if state.ctrl && state.alt && keycode == 0x10 {
@@ -178,15 +305,21 @@ pub fn handle_scancode(scancode: u8, state: &mut KeyboardState, output: &mut [u8
                     };
                 }
 
-                let mut key = match state.lang {
+                let caps_flag = state.capslock || state.shift;
+                let altgr_flag = state.alt;
+                let verrnum_flag = state.verrnum;
+                let keymap_index = ((verrnum_flag as usize) << 10)
+                    | ((altgr_flag as usize) << 9)
+                    | (caps_flag as usize) << 8
+                    | keycode as usize;
+
+                let keymap = match state.lang {
                     KeymapLanguage::US => KEYMAP_US,
                     KeymapLanguage::FR => KEYMAP_FR,
-                }[keycode as usize];
+                };
 
-                if state.capslock || state.shift {
-                    key = key.to_ascii_uppercase();
-                }
-
+                let mut key = keymap[keymap_index as usize];
+                // println!("Key: {}", keycode as usize);
                 if key != 0 {
                     if state.ctrl {
                         if !key.is_ascii_alphabetic() {
